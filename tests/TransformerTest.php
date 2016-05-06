25d82369362897cd835e98f61216a0fae2ff363d
Jason Lam <jasonlam604@gmail.com>
2016-05-05 18:03:47 -0700

    Add Transform unit test

---
+++ tests/TransformerTest.php
@@ -0,0 +1,25 @@
+<?php
+use Stringizer\Stringizer;
+use Stringizer\Transformers\Transformer;
+
+/**
+ * Transformer Unit Tests
+ */
+class TransformerTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testTransformer()
+    {
+        $t = new Transformer("dummy-value");
+        $this->assertEquals("dummy-value", $t->getValue());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testTransformerNullException()
+    {
+        $t = new Transformer(null);
+
+    }
+}
