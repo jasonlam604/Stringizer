049bca02c6f6e8bf861dc682c7e2c6a449e21b0f
Jason Lam <jasonlam604@gmail.com>
2016-05-30 21:30:03 -0700

    Fix incorrect test

--- tests/Ipv6Test.php
+++ tests/Ipv6Test.php
@@ -10,15 +10,15 @@ class Ipv6Test extends PHPUnit_Framework_TestCase
     public function testValidIp()
     {
         $s = new Stringizer("2001:cdba:0000:0000:0000:0000:3257:9652");
-        $this->assertEquals(true, $s->isIpv4());
+        $this->assertEquals(true, $s->isIpv6());
     }
 
     public function testInValidIp()
     {
         $s = new Stringizer("xx");
-        $this->assertEquals(false, $s->isIpv4());
-        
+        $this->assertEquals(false, $s->isIpv6());
+
         $s = new Stringizer("2001:cdba:0000:0000:0000:0000:3257:9652x");
-        $this->assertEquals(false, $s->isIpv4());
+        $this->assertEquals(false, $s->isIpv6());
     }
 }
